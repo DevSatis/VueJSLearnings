aac2fc18badbbe725c9b36cc9d795640dc5cd4c8
DevSatis <sksatishk9@gmail.com>
2023-03-18 23:16:34 +0530

    SmallProgress

--- vueJs/src/App.vue
+++ vueJs/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
    <Greet />
+   <Test />
   </div>
    
 </template>
 
 <script >
  import Greet from "./components/Greet.vue"; 
+ import Test from "./components/Test.vue";
 
  export default{
   name: "App",
   components:{
     Greet,
+    Test,
 
   }
  }
---
+++ vueJs/src/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h3>I'm Learning Vue JS, as per the requirement.</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test",
+    }
+</script>
